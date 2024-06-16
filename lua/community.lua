21b2399daaa73c2501616da296e9e6a72e12fe1a
nattrio <jirapat.nattrio@gmail.com>
2024-06-17 01:28:39 +0700

    CHORE update langs and tools

--- lua/community.lua
+++ lua/community.lua
@@ -5,10 +5,18 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Programming Languages
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescripts" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.yaml" },
+  -- Frameworks
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.docker" },
+  -- Tools
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
 }
