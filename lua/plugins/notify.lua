3f2bfa7758dd2c304938023e9ee1c36ad1849a5c
nattrio <jirapat.nattrio@gmail.com>
2024-06-18 00:50:39 +0700

    chore: Update notify.lua to use wrapped-compact rendering and set minimum and maximum width

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -11,15 +11,14 @@ return
         },
     },
     opts = {
-        render = "default",
+        render = "wrapped-compact",
         stages = "fade_in_slide_out",
         timeout = 3000,
         top_down = false,
+        minimum_width = 50,
+        max_width = 50,
         max_height = function()
             return math.floor(vim.o.lines * 0.75)
-        end,
-        max_width = function()
-            return math.floor(vim.o.columns * 0.75)
         end,
         on_open = function(win)
             vim.api.nvim_win_set_config(win, { zindex = 100 })
